b89943f9abdcb489230b812549bb3336490d539f
Patrick Long <patrick.long@gmail.com>
2019-06-24 07:24:43 -0400

    setup tableViewController

---
+++ Data Science Study App/DataScienceTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DataScienceTableViewController.swift
+//  Data Science Study App
+//
+//  Created by patrick long on 6/24/19.
+//  Copyright © 2019 patrick long. All rights reserved.
+//
+
+import UIKit
+
+class DataScienceTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel?.text = "Hello World"
+        
+        return cell
+    }
+
+}
